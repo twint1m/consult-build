ae13734adb216548b7927784df25f1fcb3ce73ce
twint1m <twint1m.contact@gmail.com>
2024-10-19 10:57:17 +0300

    deleted group & discord

--- frontend/src/components/Form/hook.ts
+++ frontend/src/components/Form/hook.ts
@@ -1,22 +1,20 @@
-import {StudentInfo, StudentsData} from "./interface";
-import {useEffect, useState} from "react";
+import { StudentInfo, StudentsData } from "./interface";
+import { useEffect, useState } from "react";
 import studentsData from '../../data/students.json';
 
 export const useFindMentor = () => {
     const [studentName, setStudentName] = useState<string>('');
-    const [group, setGroup] = useState<string>('');
     const [mentor, setMentor] = useState<string>('');
 
     useEffect(() => {
         const normalizeString = (str: string) => str.trim().toLowerCase();
 
-        if (studentName && group) {
+        if (studentName) {
             let foundMentor = 'Студент не найден';
             const normalizedStudentName = normalizeString(studentName);
-            const normalizedGroup = normalizeString(group);
 
-            const groupData = (studentsData as StudentsData)[normalizedGroup];
-            if (groupData) {
+            for (const group in studentsData) {
+                const groupData = studentsData[group];
                 for (const mentorKey in groupData) {
                     const studentGroup = groupData[mentorKey];
                     const studentInfo = studentGroup.find((student: StudentInfo) => {
@@ -28,10 +26,12 @@ export const useFindMentor = () => {
                         break;
                     }
                 }
+                if (foundMentor !== 'Студент не найден') break;
             }
+
             setMentor(foundMentor || 'Наставник не найден');
         }
-    }, [studentName, group]);
+    }, [studentName]);
 
-    return { studentName, setStudentName, group, setGroup, mentor };
+    return { studentName, setStudentName, mentor };
 };
--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -5,38 +5,30 @@ import {
   FormGroup,
   Label,
   Input,
-  Select,
   TextArea,
-  CheckboxGroup,
   Button,
-  CheckboxInput,
-  CheckboxLabel,
   MentorFormGroup,
   MentorLabel,
   MentorSpan,
   Link,
-  ProblemButton
 } from "./styles";
 import { useFindMentor } from "./hook";
 import { Outlet } from "react-router-dom";
 import Toaster from "../ui/Toaster";
 
 const ConsultationForm: React.FC = () => {
-  const { studentName, setStudentName, group, setGroup, mentor } = useFindMentor();
+  const { studentName, setStudentName, mentor } = useFindMentor();
   const [topic, setTopic] = useState("");
   const [comments, setComments] = useState("");
-  const [discord, setDiscord] = useState("");
   const [toast, setToast] = useState<{ message: string; type: "success" | "error" } | null>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const data = {
       student_name: studentName,
-      group,
       mentor,
       topic,
       comments,
-      discord,
     };
 
     try {
@@ -54,17 +46,11 @@ const ConsultationForm: React.FC = () => {
 
       setToast({ message: "Вы записаны на консультацию!", type: "success" });
       setStudentName("");
-      setGroup("");
       setTopic("");
       setComments("");
-      setDiscord("");
     } catch (error) {
       setToast({ message: "Ошибка при отправке данных", type: "error" });
     }
-  };
-
-  const handleProblemReport = () => {
-    window.location.href = "https://github.com/twint1m/consult-build/issues/new";
   };
 
   const closeToast = () => {
@@ -86,20 +72,6 @@ const ConsultationForm: React.FC = () => {
                 placeholder={"Пример: Шкредов Антон Алеексеевич"}
                 onChange={(e) => setStudentName(e.target.value)}
             />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="group">Группа</Label>
-            <Select
-                required
-                value={group}
-                onChange={(e) => setGroup(e.target.value)}
-            >
-              <option value="" disabled>
-                Выберите группу
-              </option>
-              <option value="241-321">241-321</option>
-              <option value="241-3210">241-3210</option>
-            </Select>
           </FormGroup>
           <MentorFormGroup>
             <MentorLabel htmlFor="mentor">Ваш наставник:</MentorLabel>
@@ -129,31 +101,8 @@ const ConsultationForm: React.FC = () => {
                 onChange={(e) => setComments(e.target.value)}
             />
           </FormGroup>
-          <FormGroup>
-            <Label htmlFor="discord">Контакт в дискорде</Label>
-            <Input
-                type="text"
-                id="discord"
-                name="discord"
-                placeholder={"В формате @username"}
-                required
-                value={discord}
-                onChange={(e) => setDiscord(e.target.value)}
-            />
-          </FormGroup>
-          <CheckboxGroup>
-            <CheckboxInput type="checkbox" id="consent" name="consent" required />
-            <CheckboxLabel htmlFor="consent">
-              Подтверждаю
-              <Link href={"/consent"}>согласие</Link>
-              на обработку персональных данных
-            </CheckboxLabel>
-          </CheckboxGroup>
           <Button type="submit">Отправить</Button>
         </form>
-        {/*<ProblemButton type="button" onClick={handleProblemReport}>*/}
-        {/*  Сообщить о проблеме*/}
-        {/*</ProblemButton>*/}
         {toast && <Toaster message={toast.message} type={toast.type} onClose={closeToast} />}
         <Outlet />
       </Container>
